94204f4145d9f6f2b75a3b8e2cd5da997f0826b3
gabbeeto <124828006+gabbeeto@users.noreply.github.com>
2025-03-31 18:14:06 -0300

    add assets that I will use

--- src/Food.jsx
+++ src/Food.jsx
@@ -1,7 +1,9 @@
-export default function Food({source, name}) {
+
+export default function Food({imgSrc,modelSrc, name}) {
 	return (<>
 		<figure>
-			<img src={source} className="font-light dark:text-red-300 text-red-950" alt={`imagen para ${name}`}/>
+		<ModelViewer src={modelSrc}/>
+			<img src={imgSrc} className="font-light dark:text-red-300 text-red-950" alt={`imagen para ${name}`}/>
 	 <figcaption className="font-normal">{name}</figcaption>
 		</figure>
 	</>)
