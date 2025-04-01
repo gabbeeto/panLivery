54cf984182cecb33e0a0107622cfaa942001c8d9
gabbeeto <124828006+gabbeeto@users.noreply.github.com>
2025-04-01 15:51:11 -0300

    add prices and make website look better

--- src/MainElement.jsx
+++ src/MainElement.jsx
@@ -13,7 +13,8 @@ const imagesForFoods = {
 
 export function MainElement() {
 	const [foods, changeFoods] = useState(() => [<><h2>Cargando...</h2></>])
-	const url = "https://script.google.com/macros/s/AKfycbzIGv6gydK7oGQROIoXhiUbf63Q_-rWteY-bF_m_4RbHBiPTxbzbciYS_hbh9YzowIn/exec"
+	const url = "https://script.google.com/macros/s/AKfycbxS4WBLgbxPxniLrl5dxqdbxtpPdZhgPLyCd0IrkOV0cCsHSIqLBUa57LgVFa-N6gNm/exec"
+
 	useEffect(() => {
 		fetch(url).then(response => response.json())
 			.then(json => {
@@ -23,22 +24,73 @@ export function MainElement() {
 					let food = json['data'][index]
 					let foodName = food['name']
 					let foodAmount = food['amount']
-					if (index > 0 && foodAmount > 0) {
-						console.log(food)
-						Foods.push((<li key={index - 1}>
+					let price = food['pricePerUnit']
+					let thereisFoodAvailable = foodAmount > 0
+					console.log(food)
+					if (thereisFoodAvailable) {
+						Foods.push((<li className="border-black dark:border-white border-2 pb-6 rounded-4xl gap-2" key={index - 1}>
 							<Food name={foodName} source={imagesForFoods[foodName]} />
-							<p className="font-light">{foodAmount}</p>
+							<p className="dark:text-blue-200 text-blue-900 text-2xl bg-amber-200 dark:bg-amber-900 font-extrabold">{foodAmount}</p>
+
+							<p className="text-green-800 dark:text-green-200 font-bold">{price}$u </p>
+							<p className="text-green-800 dark:text-green-200 font-bold">{price * 6}$ la media docena </p>
+							<p className="text-green-800 dark:text-green-200 font-bold">{price * 12}$ la docena </p>
 						</li>))
 						changeFoods(Foods)
 					}
 				}
 			})
+
+		let output = document.querySelector('output')
+
+		let now = new Date()
+		let dayOfWeek = now.getDate()
+		let hour = now.getHours()
+		let minutes = now.getMinutes()
+
+
+		let hoursLeft = 15 - hour;
+		let minutesLeft = 60 - minutes;
+		if (dayOfWeek == 5) {
+			output.innerHTML = `faltan 2 dias para abrir`;
+		}
+		else if (dayOfWeek == 6) {
+			output.innerHTML = `faltan 1 dia para abrir`;
+		}
+		else if (hour < 14) {
+			output.innerHTML = `faltan ${hoursLeft} horas y ${minutesLeft} minutos para abrir`;
+		}
+		if (hour == 14) {
+			output.innerHTML = `faltan una hora y ${minutesLeft} minutos para abrir`;
+		}
+
+		if (hour == 15) {
+			output.innerHTML = `faltan ${minutesLeft} minutos para abrir`;
+		}
+
+		if (hour > 15) {
+			output.innerHTML = `local abierto`;
+		}
+
+
+		// console.log(minutes)
+		if (dayOfWeek > 4) {
+			// console.log(hour)
+		}
+
 	}, []);
 
 	return (<>
-		<section>
-			<h2 className="text-4xl font-bold">comida disponible</h2>
-			<ul>
+		<section className="border-black dark:border-white border-2 rounded-3xl pb-5 pt-5 mt-1 mx-2">
+			<h2 className="text-4xl font-bold">Está abierto de lunes a viernes de 16:00 hasta 20:00</h2>
+			<div className="bg-amber-200 dark:bg-amber-900">
+				<p className="text-4xl font-bold">Estado:</p>
+				<output></output>
+			</div>
+		</section>
+		<section className="border-black dark:border-white border-2 gap-2 flex flex-col rounded-3xl pb-5 pt-5 mt-1 mx-2 p-2">
+			<h2 className="text-4xl dark:text-blue-200 text-blue-900 font-extrabold">comida disponible</h2>
+			<ul className="flex flex-wrap gap-2 lg:flex-row flex-col">
 				{foods}
 			</ul>
 		</section>
--- src/main.jsx
+++ src/main.jsx
@@ -9,10 +9,12 @@ const theme = 'dark:bg-yellow-950 bg-orange-150 dark:border-white border-black d
 createRoot(document.getElementById('root')).render(<>
 	<header className={`${theme} border-b-2 p-3   ${center}`}>
 		<h1 className=' text-6xl font-extrabold'>PanLivery</h1>
-		<img src={logo} alt="logo para PanLivery self-center col-flex" className='w-18 h-18' />
+		<img src={logo} alt="logo para PanLivery" className='w-18 h-18' />
 	</header>
-	<main className={`${theme} ${center}`}>
+
+	<main className={`${theme} gap-3 flex-col ${center}`}>
 		<MainElement />
 	</main>
 </>
 )
+
